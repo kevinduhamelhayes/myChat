7c1075fdbf38a48190b71e0cec2fdea35f3af098
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-11-01 17:04:28 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import  express  from "express";
+import logger from "morgan";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html"); 
+} );
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+} );
