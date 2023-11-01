0e9a3310f8c9a52ec24c9b62f5d5a0a7e8712e21
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-11-01 18:28:15 -0300

    second commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
 import  express  from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconect", () => {
+    console.log("a user disconnected");
+  } );
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  } );
+
+}
+);
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html"); 
 } );
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening at ${port}`);
 } );
